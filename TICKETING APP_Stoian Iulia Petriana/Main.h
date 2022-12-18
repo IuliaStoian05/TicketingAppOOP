812a0cf3b14da0d3464553de6a1977c5fc1e22fb
IuliaStoian05 <72345006+IuliaStoian05@users.noreply.github.com>
2022-12-18 20:16:18 +0200

    idk

---
+++ TICKETING APP_Stoian Iulia Petriana/Main.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Main.h"
+#include "Eveniment.h"
+#include "Bilet.h"
+#include "User.h"
+#include <string>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Main
+{
+};
+
