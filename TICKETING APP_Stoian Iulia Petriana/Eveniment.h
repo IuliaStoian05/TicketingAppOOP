838ffba079cf0bbb87fbe9e54dd7e064b5e068c9
IuliaStoian05 <72345006+IuliaStoian05@users.noreply.github.com>
2022-12-18 20:50:01 +0200

    idk

---
+++ TICKETING APP_Stoian Iulia Petriana/Eveniment.h
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma once
+#include <string>
+#include<iostream>
+#include<vector>
+#include "Bilet.h"
+
+using namespace std;
+
+class Eveniment {
+private:
+	string denumire;
+	string data;
+	int nrLocuriDisponibile;
+	int nrTotalRanduri;
+	int nrTotalLocuriPeRand;
+	int nrTotalLocuri;
+	char* tipEveniment;
+
+public:
+	// destructor
+	~Eveniment();
+
+	// constructor fara parametri
+	Eveniment();
+
+	// constructor cu 4 parametri
+	Eveniment(char* tipEveniment, string denumire, string data, int nrLocuriDisponibile);
+	
+	// setteri
+	void setDenumire(string denumire);
+	void setData(string data);
+	void setNrLocuriDisponibile(int nrLocuriDisponibile);
+	void setNrTotalLocuri(int nrTotalLocuri);
+	void setNrTotalRanduri(int nrTotalRanduri);
+	void setNrTotalLocuriPeRand(int nrTotalLocuriPeRand);
+	void setTipEveniment(char* tipEveniment);
+	
+	// getteri
+	string getDenumire();
+	string getData();
+	int getNrLocuriDisponibile();
+	int getNrTotalLocuri();
+	int getNrTotalRanduri();
+	int getNrTotalLocuriPeRand();
+	char* getTipEveniment();
+	
+	// operatorul <<
+	friend ostream& operator<<(ostream& out, const Eveniment& e);
+
+	// operatorul >>
+	friend istream& operator>>(istream& in, Eveniment& e);
+
+	// operator ==
+	bool operator==(Eveniment e);
+
+	// operator ++ (post)
+	Eveniment operator++(int);
+
+	//metode
+
+	string toString();
+	void calculeazaNrLocuriTotal(int nrLocuriPeRand, int nrTotalRanduri);
+//	int calculeazaNrLocuriDisponibile(int nrTotalLocuri, vector<Bilet*> bileteVandute);
+};
+	
---
+++ TICKETING APP_Stoian Iulia Petriana/Main.cpp
@@ -0,0 +1,13 @@
+#include "Main.h"
+#include "Eveniment.h"
+#include "Bilet.h"
+#include "User.h"
+#include <string>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void main() {
+
+}
