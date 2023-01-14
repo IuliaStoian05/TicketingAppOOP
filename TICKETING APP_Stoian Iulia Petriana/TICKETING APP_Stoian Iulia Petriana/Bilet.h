777f85ee708e7c954cd9a5a3a2c205965ffa4064
IuliaStoian05 <72345006+IuliaStoian05@users.noreply.github.com>
2023-01-14 23:02:08 +0200

    v2.0

---
+++ TICKETING APP_Stoian Iulia Petriana/TICKETING APP_Stoian Iulia Petriana/Bilet.h
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma once
+#include <string>
+#include<iostream>
+#include<vector>
+#include "Eveniment.h"
+#include "IBilet.h"
+
+
+using namespace std;
+
+class Bilet: public IBilet
+{
+private:
+	const int cod;
+	char* tipLoc; // ex. VIP, normal, loja etc.
+	double pret;
+	int* loc;
+	int* rand;
+	bool esteValid; 
+	static int contorBilete;
+public:
+//	Eveniment eveniment;
+	// destructor
+	~Bilet();
+
+	// constructori
+	Bilet();
+	Bilet(const int cod, double pret);
+	
+	// setteri
+	void setTipLoc(char* tipLoc);
+	void setPret(double pret);
+	void setLoc(int* loc);
+	void setRand(int* rand);
+	void setEsteValid(bool esteValid);
+	
+	// getteri
+	int getCod();
+	char* getTipLoc();
+	double getPret();
+	int* getLoc();
+	int* getRand();
+	bool getEsteValid();
+
+	// operatorul <<
+	friend ostream& operator<<(ostream& out, const Bilet& b);
+
+	// operatorul >>
+	friend istream& operator>>(istream& in, Bilet& b);
+
+	// operator de indexare []
+	int& operator[](int pozitie);
+
+	// operator negare !
+	bool operator!();
+
+	//metode
+	string toString();
+	void verificaValiditate(vector<Bilet*> bileteValide, Bilet* b1, Bilet* b2);
+	virtual void seteazaAtribute();
+};
+
+
---
+++ TICKETING APP_Stoian Iulia Petriana/TICKETING APP_Stoian Iulia Petriana/PiesaTeatru.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<iostream>
+#include"Eveniment.h"
+#include<string>
+
+using namespace std;
+
+class PiesaTeatru: public Eveniment
+{
+private:
+	string teatru;
+	string oras;
+	double oraIncepere;
+	
+public:
+
+};
+
